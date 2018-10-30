11c75c5a23d0014dfe353b2e5f2b5b631edcfae1
Pawel Paruzel <pawelparuzel95@gmail.com>
2018-10-30 11:14:45 +0100

    Load JSON tilemap
    * added basic collision
    * tilemap assets from pokemon

--- src/game.js
+++ src/game.js
@@ -15,10 +15,20 @@ const game = new Phaser.Game(config);
 
 function preload()
 {
+    this.load.image('tiles', '../assets/tiles/blackvolution.png');
+    this.load.tilemapTiledJSON({
+        key: 'map',
+        url: '../assets/tiles/blackvolution.json',
+    });
 }
 
 function create()
 {
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('bvtiles', 'tiles');
+    const walkable_l = map.createStaticLayer('walkable', tileset, 0, 0);
+    const world_l = map.createStaticLayer('world', tileset, 0, 0);
+    const above_l = map.createStaticLayer('above', tileset, 0, 0);
 }
 
 function update()

36b6a9887c1a3d566d17914aa48ee86f034836d5
Marcin <cinekqw1@gmail.com>
2018-11-20 17:43:34 +0100

    add settings button

--- src/menu.js
+++ src/menu.js
@@ -9,7 +9,7 @@ class MainMenu extends Phaser.Scene{
 
     this.text = this.add.text(100, 100, 'Welcome to the game!', { fill: '#ffffff' });
 
-    this.clickButton = this.add.text(100, 150, 'Play!', { fill: '#0f0' })
+    this.clickButton = this.add.text(100, 150, '* Play!', { fill: '#0f0' })
      .setInteractive()
      .on('pointerdown', function (event) {
         this.scene.start('game');
@@ -20,6 +20,17 @@ class MainMenu extends Phaser.Scene{
      .on('pointerout', function (event) {
        this.clickButton.setStyle({ fill: '#0f0' });
      },this );
+
+     this.clickButton2 = this.add.text(100, 180, '* Settings', { fill: '#0f0' })
+      .setInteractive()
+      .on('pointerdown', function (event) {
+       }, this)
+      .on('pointerover', function (event) {
+        this.clickButton2.setStyle({ fill: '#ff5'});
+      }, this)
+      .on('pointerout', function (event) {
+        this.clickButton2.setStyle({ fill: '#0f1' });
+      },this );
 
   }
 
